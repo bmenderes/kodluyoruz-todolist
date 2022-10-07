8cec22e79139e1a4f347ec0375576309cd563c51
baris menderes <bmenderes@gmail.com>
2022-10-07 16:11:38 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,11 @@
+let ulDOM = document.querySelector("#list")
+let btnDOM = document.querySelector("#liveToastBtn")
+let inputDOM = document.querySelector("#task")
+
+    btnDOM.addEventListener("click", addItem)
+   
+
+    function addItem() {
+        console.log("Butona basildi")
+
+    }   
